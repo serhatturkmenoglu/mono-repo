c77c30e5f4b93d55ab52471fa71d6b0ce2f1a604
Serhat Türkmenoğlu <serhatturkmenoglu@gmail.com>
2022-07-31 16:45:15 +0300

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,6 +11,7 @@ app.get('/data', (req, res) => {
   res.send({ data: data() });
 });
 
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
