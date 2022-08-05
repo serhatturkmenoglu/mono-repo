e002720f19914640328a0bf0d04579e3ae9628a1
Serhat Türkmenoğlu <serhatturkmenoglu@gmail.com>
2022-08-05 10:09:29 +0300

    frontend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,6 +14,7 @@ app.get('/data', (req, res) => {
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
+  
   console.log(`Listening at http://localhost:${port}/api`);
 });
 server.on('error', console.error);
