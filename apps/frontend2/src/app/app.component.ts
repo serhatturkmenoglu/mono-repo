ea4a35cb5b7f73be69db571a0b2673a319722212
Serhat Türkmenoğlu <serhatturkmenoglu@gmail.com>
2022-08-07 23:19:15 +0300

    nx build

--- apps/frontend2/src/app/app.component.ts
+++ apps/frontend2/src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'mono-repo-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend2';
 
   hello(){
     console.log("hello")
+  }
+  ngOnInit(): void {
+    console.log("frontend 2 working");
   }
 }
