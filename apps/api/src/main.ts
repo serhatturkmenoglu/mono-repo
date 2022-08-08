e9cb1e79c7b9a808eaeededf162afbfb60e6ffcb
Serhat Türkmenoğlu <serhatturkmenoglu@gmail.com>
2022-08-08 16:08:26 +0300

    api build

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,14 @@
 import * as express from 'express';
-import { data } from '@mono-repo/data';
-import { inspect } from 'util';
 
 const app = express();
 app.get('/ready', (req, res) => {
   res.send({ message: 'Api is ready' });
+  
 });
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
-app.get('/data', (req, res) => {
-  console.log('/data ',inspect(data));
-  
-  res.send({ data: data() });
-});
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   
